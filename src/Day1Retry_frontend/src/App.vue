05dc323eddd3ebbfa3278183386932f8759b7ce7
No-uIsObsolete <andrzejf2007@gmail.com>
2024-06-25 22:03:01 +0200

    Added Counter

--- src/Day1Retry_frontend/src/App.vue
+++ src/Day1Retry_frontend/src/App.vue
@@ -3,6 +3,23 @@
     <img src="/logo2.svg" alt="DFINITY logo" />
     <br />
     <br />
-      Hej Blockchain!
+      Hej Blockchain! <br />
+      <br /> <button @click="podbij ">Podbij</button> <br />
+      {{ licznik }}
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
